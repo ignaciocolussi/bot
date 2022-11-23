de40eb9c806bea7b781368dc3bcb8f9b17f7688c
Ignacio Colussi <ignaciocolussi@outlook.com>
2022-11-23 08:23:57 -0300

    Se agrega cotizacion, horarios y palabrasd en final

--- src/datos/intenciones.js
+++ src/datos/intenciones.js
@@ -1,10 +1,10 @@
 const intenciones = {
     clima: ["como", "estara", "el", "clima", "hoy", "como", "estara", "el", "clima", "mañana"],
     tabla: ["tabla", "de", "posiciones", "del", "campeonato", "de", "españa", "argentina", "urugay", "como", "esta", "la"],
-    cotizacion:["cotizacion", "del", "como", "cotiza", "precio", "de", "bitcoin", "cardano", "la"],
-    horarios: [ "cual", "es", "la", "fecha", "en", "rosario", "montevideo", "madrid", "cual", "es", "la", "hora", "en",],
+    cotizacion:["cotizacion", "del", "como", "cotiza", "precio", "de", "bitcoin", "cardano", "la","cuanto", "esta", "el"],
+    horarios: [ "cual", "es", "la", "fecha", "en",  "cual", "es", "la", "hora", "en",],
     saludo: ["hola", "buen", "dia", "buenos", "dias"],
-    final: ["chau", "hasta", "luego"],
+    final: ["chau", "hasta", "luego", "adios"],
     agradecimiento: ["gracias"]
 }
 
