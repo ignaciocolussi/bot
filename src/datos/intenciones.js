b84d7a5cc428cca48cec33d40c173de9710cf3f6
Ignacio Colussi <ignaciocolussi@outlook.com>
2022-11-23 20:24:21 -0300

    fixes

--- src/datos/intenciones.js
+++ src/datos/intenciones.js
@@ -1,5 +1,5 @@
 const intenciones = {
-    clima: ["como", "estara", "el", "clima", "hoy", "como", "estara", "el", "clima", "mañana"],
+    clima: ["como", "estara", "el", "clima", "hoy", "en", "como", "estara", "el", "clima", "mañana", "en"],
     tabla: ["tabla", "de", "posiciones", "del", "campeonato", "de", "españa", "argentina", "urugay", "como", "esta", "la"],
     cotizacion:["cotizacion", "del", "como", "cotiza", "precio", "de", "bitcoin", "cardano", "la","cuanto", "esta", "el"],
     horarios: [ "cual", "es", "la", "fecha", "en",  "cual", "es", "la", "hora", "en",],
